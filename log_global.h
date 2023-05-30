3ebdc91e425127c499a14e0340007b0098e0b712
Laptop-Legion-CMG\CMGeorge <george.calugar@reea.net>
2023-05-30 08:32:10 +0300

    WIndows build fix

--- log_global.h
+++ log_global.h
@@ -2,11 +2,13 @@
 #define LOG_GLOBAL_H
 
 #include <QtCore/qglobal.h>
-
+#ifndef QT_STATIC
 #if defined(LOG_LIBRARY)
 #  define LOG_EXPORT Q_DECL_EXPORT
 #else
 #  define LOG_EXPORT Q_DECL_IMPORT
 #endif
-
+#else
+#define LOG_EXPORT
+#endif
 #endif // LOG_GLOBAL_H
